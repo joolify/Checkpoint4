15acadad70e63edcdaaf60794fb12fe298734c32
Joel Wiklund <joelwe@gmail.com>
2018-11-02 15:58:00 +0100

    Soldier of Execution

--- Checkpoint4/Checkpoint4/Program.cs
+++ Checkpoint4/Checkpoint4/Program.cs
@@ -8,6 +8,7 @@ namespace Checkpoint4
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Best movies ever!");
+            Console.WriteLine("Soldier of Execution");
         }
     }
 }
